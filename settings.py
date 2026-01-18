5111f18c2e473b9cb88d32556bbb3c9b03c54780
jonyhdtv <52671600+jonyhdtv@users.noreply.github.com>
2026-01-18 10:35:47 +0100

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,20 @@
+INSTALLED_APPS = [
+'django.contrib.admin',
+'django.contrib.auth',
+'django.contrib.contenttypes',
+'django.contrib.sessions',
+'django.contrib.messages',
+'django.contrib.staticfiles',
+'repairs',
+]
+
+
+LANGUAGES = [
+('pl', 'Polski'),
+('en', 'English'),
+('de', 'Deutsch'),
+]
+
+
+LANGUAGE_CODE = 'pl'
+USE_I18N = True
